5c7503746c366a1f55923b23c7f82abfac9ab59f
TomTork <dima26483950@gmail.com>
2023-02-13 02:00:20 +0500

    ver. 3.2 modify Cipher

--- main.py
+++ main.py
@@ -44,14 +44,6 @@ class Cipher:
         return [((f[i]) ^ (~s | ~i)) & ((f[i] & ~(s2 ^ i)) ^ key1[(i ^ ~f[i]) % 16])
                 & (key1[(i ^ f[i]) % 16] | s2) for i in range(16)]
 
-    def to_format(self, k):
-        try:
-            for i in range(16 - len(k)):
-                k.append((self.second[(~i | self.second[(i ^ 24) % 16]) % 16] + i))
-            return k
-        except BaseException:
-            return k
-
     @staticmethod
     def func(value, n1, n2):
         return (value ^ n1) ^ n2
@@ -70,13 +62,17 @@ class Cipher:
         return ~(~value ^ k2)
 
     @staticmethod
+    def eq(value, k4):
+        return 1 ^ value ^ k4
+
+    @staticmethod
     def rotate_List1(arry, E, K):
         arry[:] = arry[E:K] + arry[0:E]
         return arry
 
     def encrypt(self, text, key):
         text_mas = [ord(i) for i in self.b64e(text)]
-        key_mas = self.to_format([ord(i) for i in (hash_code(key))])
+        key_mas = [ord(i) for i in (hash_code(key))]
         hash_mas = [ord(i) for i in hash_code(self.b64e(key))]
         massive_generated_keys = [key_mas]
         leTextMas = len(text_mas)
@@ -86,8 +82,7 @@ class Cipher:
                 massive_generated_keys.append(
                     list(map(lambda x: (x ^ self.first[i]) % 126 + 1, self.xor_massive(massive_generated_keys[i - 1],
                                                                                        self.first[i],
-                                                                                       self.second[i], key_mas)))
-                )
+                                                                                       self.second[i], key_mas))))
             i += 1
             gMas = massive_generated_keys[gIndex]
             for index in range(leTextMas):
@@ -95,18 +90,22 @@ class Cipher:
                 text_mas[index] ^= gMas[gIndex]
                 text_mas[index] ^= key_mas[gIndex]
                 text_mas[index] = self.func2(text_mas[index], key_mas[gIndex])
+                if index <= leTextMas // 2 and gIndex % 2 == 0:
+                    text_mas[index] = ~text_mas[index]
+                elif gIndex % 2 != 0:
+                    text_mas[index] = ~text_mas[index]
                 text_mas[index] = self.func3(text_mas[index], key_mas[-gIndex])
                 text_mas[index] = self.func4(text_mas[index], key_mas[gIndex])
+                text_mas[index] ^= hash_mas[gIndex]
+                text_mas[index] = self.eq(text_mas[index], key_mas[gIndex])
                 text_mas[index] ^= key_mas[-gIndex]
-                key_mas = self.rotate_List1(key_mas, gIndex, 32)
-                text_mas = self.rotate_List1(text_mas, (gIndex + hash_mas[gIndex] + hash_mas[-gIndex]) % (leTextMas - 1), leTextMas)
             hash_mas = self.rotate_List1(hash_mas, 2, 32)
             hash_mas = [(i ^ (self.second[gIndex] + 1) + 1) for i in hash_mas]
         return ' '.join([str(i) for i in text_mas])
 
     def decrypt(self, text, key):
         text_mas = [int(i) for i in text.split()]
-        key_mas = self.to_format([ord(i) for i in (hash_code(key))])
+        key_mas = [ord(i) for i in (hash_code(key))]
         hash_text_mas = [ord(i) for i in hash_code(self.b64e(key))]
         massive_generated_keys = [key_mas]
         leTextMas = len(text_mas)
@@ -116,8 +115,7 @@ class Cipher:
                 massive_generated_keys.append(
                     list(map(lambda x: (x ^ self.first[i]) % 126 + 1, self.xor_massive(massive_generated_keys[i - 1],
                                                                                        self.first[i],
-                                                                                       self.second[i], key_mas)))
-                )
+                                                                                       self.second[i], key_mas))))
             i += 1
             gMas = massive_generated_keys[gIndex]
             for index in range(leTextMas):
@@ -125,11 +123,15 @@ class Cipher:
                 text_mas[index] ^= gMas[gIndex]
                 text_mas[index] ^= key_mas[gIndex]
                 text_mas[index] = self.func2(text_mas[index], key_mas[gIndex])
+                if index <= leTextMas // 2 and gIndex % 2 == 0:
+                    text_mas[index] = ~text_mas[index]
+                elif gIndex % 2 != 0:
+                    text_mas[index] = ~text_mas[index]
                 text_mas[index] = self.func3(text_mas[index], key_mas[-gIndex])
                 text_mas[index] = self.func4(text_mas[index], key_mas[gIndex])
+                text_mas[index] ^= hash_text_mas[gIndex]
+                text_mas[index] = self.eq(text_mas[index], key_mas[gIndex])
                 text_mas[index] ^= key_mas[-gIndex]
-                key_mas = self.rotate_List1(key_mas, gIndex, 32)
-                text_mas = self.rotate_List1(text_mas, (gIndex + hash_text_mas[gIndex] + hash_text_mas[-gIndex]) % (leTextMas - 1), leTextMas)
             hash_text_mas = self.rotate_List1(hash_text_mas, 2, 32)
             hash_text_mas = [(i ^ (self.second[gIndex] + 1) + 1) for i in hash_text_mas]
         return self.b64d(''.join([chr(i) for i in text_mas]))
